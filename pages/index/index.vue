6346040698d097a16c5f50c95ab26f21f30ac50b
ChaseTS <3154014120@qq.com>
2022-04-30 17:45:08 +0800

    feat: 登录页面

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,10 +1,10 @@
 <template>
 	<view class="content">
-		<view class="identity-card">
+		<view class="identity-card" @tap="toLogin" data-card="student">
 			<uni-icons custom-prefix="iconfont" type="icon-xuesheng" size="40" color="#f3fec3"></uni-icons>
 			<text class="card-text">我是学生</text>
 		</view> 
-		<view class="identity-card">
+		<view class="identity-card" @tap="toLogin" data-card="teacher">
 			<uni-icons custom-prefix="iconfont" type="icon-shangkelaoshiwo" size="40" color="#f3fec3"></uni-icons>
 			<text class="card-text">我是老师</text>
 		</view>
@@ -22,29 +22,35 @@
 
 		},
 		methods: {
-
+			toLogin(e){
+				let card = e.currentTarget.dataset.card;
+				uni.navigateTo({
+					url : "../login/login?card=" + card 
+				})
+			}
 		}
 	}
 </script>
 
-<style>
+<style lang="scss" scoped>
 	.identity-card {
 		display: flex;
 		align-items: center;
 		justify-content: center;
 		width: 660upx;
 		height: 250upx;
-		margin: 100upx auto;
-		background: url("https://www.jxnu.edu.cn/_upload/site/00/05/5/logo.png") 500upx 50upx no-repeat;
-		background-color: #a13838;
+		margin: 150upx auto;
+		background: url("https://bkimg.cdn.bcebos.com/pic/b21c8701a18b87d6473830f5000828381e30fde6")no-repeat;
+		background-size: 100upx;
+		background-color: #1b478e;
 		border-radius: 20upx;
 		color: #f3fec3;
 		text-shadow: #ffffce;
 		font-size: 40upx;
 		box-shadow: 0px 1px 10px 2px rgba($color: #a5a4a4, $alpha: 0.5);
-	}
-	.identity-card:hover{
-		color: white;
+		&:hover{
+			color: white;
+		}
 	}
 	.card-text{
 		margin-left: 40upx;
---
+++ pages/login/login.vue
@@ -0,0 +1,103 @@
+<template>
+	<view class="login-content">
+		<view>
+			<image class="login-img" src="https://uis.jxnu.edu.cn/cas/file/png/logoM.png" lazy-load mode="aspectFit"
+				fade-show></image>
+		</view>
+		<view class="login-card">
+			<text class="uni-h6">账号登录</text>
+		</view>
+		<view class="login-form" v-if="optionChoose">
+			<uni-forms ref="baseForm" :modelValue="loginFormData">
+				<uni-forms-item>
+					<uni-easyinput prefixIcon=".uniui-person-filled" v-model="loginFormData.studentNumber" placeholder="请输入工号或学号" />
+				</uni-forms-item>
+				<uni-forms-item>
+					<uni-easyinput prefixIcon=".uniui-locked-filled" v-model="loginFormData.password" placeholder="请输入登录密码" />
+				</uni-forms-item>
+			</uni-forms>
+			<button type="default" class="button" @click="loginSubmit">提交</button>
+		</view>
+		<view class="login-form" v-else>
+			<uni-forms ref="baseForm" :modelValue="loginFormData">
+				<uni-forms-item>
+					<uni-easyinput prefixIcon=".uniui-person-filled" v-model="loginFormData.teacherNumber" placeholder="请输入工号或学号" />
+				</uni-forms-item>
+				<uni-forms-item>
+					<uni-easyinput prefixIcon=".uniui-locked-filled" v-model="loginFormData.password" placeholder="请输入登录密码" />
+				</uni-forms-item>
+			</uni-forms>
+			<button type="default" class="button" @click="loginSubmit">提交</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				loginFormData:{
+				},
+				optionChoose: ""
+			}
+		},
+		onLoad(option) {
+			this.optionChoose = option.card == "student" ? true : false
+			this.cardChoose(option.card)
+		},
+		computed:{
+		},
+		methods: {
+			cardChoose(card){
+				if(card == "student"){
+					this.loginFormData = {
+						"studentNumber":null,
+						"password":""
+					}
+				}else{
+					this.loginFormData = {
+						"teacherNumber":null,
+						"password":""
+					}
+				}
+			},
+			loginSubmit(){
+				console.log(this.loginFormData)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.login-content {
+		width: 700rpx;
+		display: flex;
+		justify-content: center;
+		flex-wrap: wrap;
+		margin: 0 auto;
+
+		.login-img {
+			width: 500rpx;
+			height: 200rpx;
+		}
+		.login-card{
+			color: $jxnu-bg-color;
+			text-align: center;
+			width: 700rpx;
+			margin-bottom: 40rpx;
+		}
+	}
+	
+	.login-form{
+		.uni-easyinput{
+			width: 650rpx;
+			border-radius: 100rpx;
+		}
+		.button{
+			margin-top: 50rpx;
+			background-color: $jxnu-bg-color;
+			color: $uni-text-color-inverse;
+			border-radius: 100rpx;
+		}
+	}
+</style>
